e5578a048e46d1a906275375696d7995c1c2f88c
longrunpc <pcjmo4050@gmail.com>
2026-03-07 03:17:13 +0900

    feat(domain): part,team repository에 cohortId에 해당하는 List 반환 기능 추가

--- module-domain/src/main/java/com/longrunpc/domain/cohort/repository/TeamRepository.java
+++ module-domain/src/main/java/com/longrunpc/domain/cohort/repository/TeamRepository.java
@@ -1,8 +1,11 @@
 package com.longrunpc.domain.cohort.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.longrunpc.domain.cohort.entity.Team;
 
 public interface TeamRepository extends JpaRepository<Team, Long> {
+    List<Team> findByCohortId(Long cohortId);
 }
