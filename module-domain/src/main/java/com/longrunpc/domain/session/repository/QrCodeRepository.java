d91ac5e436c7ca9e355ea3121410ecd59f311bf2
longrunpc <pcjmo4050@gmail.com>
2026-03-08 08:10:03 +0900

    test(api): 일정 목록 조회 (관리자용) usecase 테스트 작성

--- module-domain/src/main/java/com/longrunpc/domain/session/repository/QrCodeRepository.java
+++ module-domain/src/main/java/com/longrunpc/domain/session/repository/QrCodeRepository.java
@@ -4,7 +4,6 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import com.longrunpc.domain.session.entity.QrCode;
 
