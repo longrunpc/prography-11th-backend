faecedf129e1a84accb39ff12f9f63915fda8190
longrunpc <pcjmo4050@gmail.com>
2026-03-07 22:33:13 +0900

    feat(domain): Attendance repository interface 정의

---
+++ module-domain/src/main/java/com/longrunpc/domain/attendance/repository/AttendanceRepository.java
@@ -0,0 +1,11 @@
+package com.longrunpc.domain.attendance.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.longrunpc.domain.attendance.entity.Attendance;
+
+public interface AttendanceRepository extends JpaRepository<Attendance, Long> {
+    List<Attendance> findBySessionId(Long sessionId);
+}
