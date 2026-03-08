c6a0468973cf90d4a65f66c93b5d92ccd43193a4
longrunpc <pcjmo4050@gmail.com>
2026-03-08 17:20:45 +0900

    feat(api): 회원 출결 요약 api 추가

--- module-api/src/main/java/com/longrunpc/api/user/member/controller/MemberController.java
+++ module-api/src/main/java/com/longrunpc/api/user/member/controller/MemberController.java
@@ -6,7 +6,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.http.ResponseEntity;
 import com.longrunpc.common.response.ApiResponse;
+import com.longrunpc.api.user.member.usecase.ReadMemberAttendanceSummaryUsecase;
 import com.longrunpc.api.user.member.usecase.ReadMemberUsecase;
+import com.longrunpc.api.user.member.dto.response.MemberAttendanceSummaryResponse;
 import com.longrunpc.api.user.member.dto.response.MemberResponse;
 
 import lombok.RequiredArgsConstructor;
@@ -17,9 +19,15 @@ import lombok.RequiredArgsConstructor;
 public class MemberController {
 
     private final ReadMemberUsecase readMemberUsecase;
+    private final ReadMemberAttendanceSummaryUsecase readMemberAttendanceSummaryUsecase;
 
     @GetMapping("/{id}")
     public ResponseEntity<ApiResponse<MemberResponse>> readMember(@PathVariable Long id) {
         return ResponseEntity.ok(ApiResponse.success(readMemberUsecase.execute(id)));
+    }
+
+    @GetMapping("/{memberId}/attendance-summary")
+    public ResponseEntity<ApiResponse<MemberAttendanceSummaryResponse>> readMemberAttendanceSummary(@PathVariable Long memberId) {
+        return ResponseEntity.ok(ApiResponse.success(readMemberAttendanceSummaryUsecase.execute(memberId)));
     }
 }
