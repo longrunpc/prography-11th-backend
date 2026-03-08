8278dcbfb93934718677cc30166a3247e202e40d
longrunpc <pcjmo4050@gmail.com>
2026-03-08 10:46:05 +0900

    fix(domain): repository query 파라미터 인식 오류 해결

--- module-domain/src/main/java/com/longrunpc/domain/session/repository/SessionRepository.java
+++ module-domain/src/main/java/com/longrunpc/domain/session/repository/SessionRepository.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.longrunpc.domain.session.entity.Session;
 import com.longrunpc.domain.session.entity.SessionStatus;
@@ -12,8 +13,8 @@ import com.longrunpc.domain.session.entity.SessionStatus;
 public interface SessionRepository extends JpaRepository<Session, Long> {
 
     @Query("SELECT s FROM Session s WHERE s.cohort.id = :cohortId AND s.sessionStatus != :status")
-    List<Session> findByCohortIdAndSessionStatusNot(Long cohortId, SessionStatus status);
+    List<Session> findByCohortIdAndSessionStatusNot(@Param("cohortId") Long cohortId, @Param("status") SessionStatus status);
 
     @Query("SELECT s FROM Session s WHERE s.cohort.id = :cohortId AND s.sessionDate BETWEEN :dateFrom AND :dateTo AND s.sessionStatus = :sessionStatus")
-    List<Session> findByCohortIdAndSessionDateBetweenAndSessionStatus(Long cohortId, LocalDate dateFrom, LocalDate dateTo, SessionStatus sessionStatus);
+    List<Session> findByCohortIdAndSessionDateBetweenAndSessionStatus(@Param("cohortId") Long cohortId, @Param("dateFrom") LocalDate dateFrom, @Param("dateTo") LocalDate dateTo, @Param("sessionStatus") SessionStatus sessionStatus);
 }
